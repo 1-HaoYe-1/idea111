3c99d712ffafe4d2c1f073e980037ce4f6919dd9
豪也 <2359699011@qq.com>
2023-07-19 19:27:20 +0800

    第二次更新

--- src/main/java/com/atguigu/HelloGit.java
+++ src/main/java/com/atguigu/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("最帅的新建");
         System.out.println("第二次的修改");
+        System.out.println("第二次的修改");
     }
 }
